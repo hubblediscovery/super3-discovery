0e89c90936a86d9c8cb4ca45edcb21e21ccc4623
Deepak VP <deepak.vpd@gmail.com>
2013-03-17 14:00:25 -0700

    Modified Unit test to match TrendingProfile data structure.

--- hubble-store/src/test/java/com/hubble/store/AppTest.java
+++ hubble-store/src/test/java/com/hubble/store/AppTest.java
@@ -1,8 +1,14 @@
 package com.hubble.store;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import com.appdisc.trend.twitter.TwitterTrendingProfile;
 import com.hubble.store.common.request.AppRequestParameter;
 import com.hubble.store.common.util.HubbleLogger;
 import com.hubble.store.persistence.PersistenceHandler;
@@ -45,7 +51,17 @@ public class AppTest
     
     public void getAppForTrendingTopics() {
     	AppRequestParameter appRequestParam = new AppRequestParameter();
-    	List<String> inputParam = Arrays.asList("Google"); 
+    	
+    	Set<String> keywords = new HashSet<String>(Arrays.asList("iPad", "iOS"));
+    	Map<String, Set<String>> trendToKeywordMap = new HashMap<String, Set<String>>();
+    	trendToKeywordMap.put("Apple", keywords);
+    	
+    	TwitterTrendingProfile trendProfile = new TwitterTrendingProfile();
+    	trendProfile.setTrendingTopicToKeywordsMap(trendToKeywordMap);
+
+    	List<TwitterTrendingProfile> inputParam = new ArrayList<TwitterTrendingProfile>();
+    	inputParam.add(trendProfile);
+   	
     	appRequestParam.setTrendingTopics(inputParam);
     	PersistenceHandler.getAppForTrendingTopics(appRequestParam);
     }
